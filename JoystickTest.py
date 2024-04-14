68adb3d9217bbbe44b4e51bd557b1ff742447eb5
Raymond Wu <98779905+Raymond131@users.noreply.github.com>
2024-04-14 12:58:58 -0400

    Update JoystickTest.py Now it has all vlaues printed!

--- JoystickTest.py
+++ JoystickTest.py
@@ -6,10 +6,22 @@ joystick.init()
 import time
 while True:
     pygame.event.get()
+    # for event in pygame.event.get(): //comment the line right before this block before uncommenting this block
+    #     if event.type == pygame.JOYBUTTONDOWN: //this block prints all joystick events
+    #         print(event)
+    #     if event.type == pygame.JOYBUTTONUP:
+    #         print(event)
+    #     if event.type == pygame.JOYAXISMOTION:
+    #         print(event)
+    #     if event.type == pygame.JOYHATMOTION:
+    #         print(event)
+        
     ax0 = int(100*(round(joystick.get_axis(0),2)+1))
     ax1 = int(100*(round(joystick.get_axis(1),2)+1))
     ax2 = int(100*(round(joystick.get_axis(2),2)+1))
     ax3 = int(100*(round(joystick.get_axis(3),2)+1))
+    ax4 = int(100*(round(joystick.get_axis(4),2)+1))
+    ax5 = int(100*(round(joystick.get_axis(5),2)+1))
     
     bt0 = joystick.get_button(0)
     bt1 = joystick.get_button(1)
@@ -21,9 +33,6 @@ while True:
     bt7 = joystick.get_button(7)
     bt8 = joystick.get_button(8)
     bt9 = joystick.get_button(9)
-    # bt10 = joystick.get_button(10)
-    # bt11 = joystick.get_button(11)
     
-
-    print('ax0: %3d, ax1: %3d, ax2: %3d, ax3: %3d, bt0:%d, bt1:%d, bt2:%d, bt3:%d, bt4:%d, bt5:%d, bt6:%d, bt7:%d, bt8:%d, bt9:%d' % (ax0, ax1, ax2, ax3, bt0, bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9))
+    print('ax0: %3d, ax1: %3d, ax2: %3d, ax3: %3d, ax4: %3d, ax5: %3d, bt0:%d, bt1:%d, bt2:%d, bt3:%d, bt4:%d, bt5:%d, bt6:%d, bt7:%d, bt8:%d, bt9:%d' % (ax0, ax1, ax2, ax3, ax4, ax5, bt0, bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9))
     time.sleep(0.05)
